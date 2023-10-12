e64bb2afb757ff11cc2ee22d333091a881575656
arv <marvin07ar@gmail.com>
2023-10-12 16:12:49 +0700

    12/10/2023

---
+++ application/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from wtforms import StringField, EmailField, SubmitField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min = 4, max = 8)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min = 6)])
+    submit   = SubmitField("Log Up")
+
+class SignUpForm(FlaskForm):
+    username         = StringField("Username", validators=[DataRequired(), Length(min = 4, max = 8)])
+    password         = PasswordField("Password", validators=[DataRequired(), Length(min = 8)])
+    email            = EmailField("Email", validators=[DataRequired(), Length(min = 8)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password", message = "Password must match!")])
+    submit           = SubmitField("Sign Up")
+
+class EditProfile(FlaskForm):
+    new_bio          = TextAreaField("New Bio")
+    new_profile_pic  = TextAreaField("New Profile Picture")
+    submit           = SubmitField("Update Profile")
+
+class CreatePost(FlaskForm):
+    post    = TextAreaField("Post")
+    caption = TextAreaField("Caption")
+    submit  = SubmitField("Create Post")
+
+class EditPost(FlaskForm):
+    new_caption = TextAreaField("New Caption")
+    submit      = SubmitField("Update Post")
--- application/models.py
+++ application/models.py
@@ -5,34 +5,46 @@ class User(db.Model):
     __tablename__ = "users"
     id          = db.Column(db.Integer, primary_key = True)
     username    = db.Column(db.String(128), nullable = False)
+    email       = db.Column(db.String(128), nullable = False)
     password    = db.Column(db.String(128), nullable = False)
-    profile_pic = db.Column(db.String(128))
+    fullname    = db.Column(db.String(128), nullable = False)
+    profile_pic = db.Column(db.String(128), default = "default.jpg")
     bio         = db.Column(db.String(128))
+    join_date   = db.Column(db.DateTime, default = datetime.utcnow)
+    status      = db.Column(db.Boolean, default = True)
 
-class Follower(db.Model):
-    __tablename__ = "followers"
-    id           = db.Column(db.Integer, primary_key = True)
-    id_follower  = db.Column(db.Integer, nullable = False)
-    id_following = db.Column(db.Integer, nullable = False)
-    status       = db.Column(db.Boolean)
+class Relation(db.Model):
+    __tablename__ = "relations"
+    id            = db.Column(db.Integer, primary_key = True)
+    id_follower   = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    id_following  = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    status        = db.Column(db.Boolean, default = True)
+    relation_date = db.Column(db.DateTime, default = datetime.utcnow)
 
 class Post(db.Model):
     __tablename__ = "posts"
     id           = db.Column(db.Integer, primary_key = True)
-    author_id    = db.Column(db.String(128), nullable = False)
+    author_id    = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
     photo        = db.Column(db.String(128), nullable = False)
-    caption      = db.Column(db.String(128))
+    caption      = db.Column(db.String(128), default = "")
+    status       = db.Column(db.Boolean, default = True)
+    post_date    = db.Column(db.DateTime, default = datetime.utcnow)
 
 
 class Comment(db.Model):
     __tablename__ = "comments"
     id           = db.Column(db.Integer, primary_key = True)
-    text         = db.Column(db.String(1024), nullable = False)
-    commenter_id = db.Column(db.Integer, primary_key = True)
-    post_id      = db.Column(db.Integer)
+    text         = db.Column(db.Text, nullable = False)
+    commenter_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    post_id      = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable = False)
+    hidden       = db.Column(db.Boolean, default = False)
+    comment_date = db.Column(db.DateTime, default = datetime.utcnow)
+
 
 class Like(db.Model):
     __tablename__ = "likes"
-    id       = db.Column(db.Integer, primary_key = True)
-    like_id  = db.Column(db.Integer, nullable = False)
-    post_id  = db.Column(db.Integer, nullable = False)
+    id        = db.Column(db.Integer, primary_key = True)
+    user_id   = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    post_id   = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable = False)
+    status    = db.Column(db.Boolean, default = True)
+    like_date = db.Column(db.DateTime, default = datetime.utcnow)
