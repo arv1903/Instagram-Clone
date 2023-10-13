767ce3221c70db1d8f60569fcfb892b1e247c369
arv <marvin07ar@gmail.com>
2023-10-13 14:09:46 +0700

    13/10/2023

--- application/forms.py
+++ application/forms.py
@@ -1,32 +1,53 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField
+from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, EmailField, SubmitField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+from application.utils import exists_email, not_exists_email, exists_username
+    
+
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min = 4, max = 8)])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min = 6)])
-    submit   = SubmitField("Log Up")
+    username             = StringField("username", validators=[DataRequired()])
+    password             = PasswordField("password", validators=[DataRequired()])
+    submit               = SubmitField("login")
 
 class SignUpForm(FlaskForm):
-    username         = StringField("Username", validators=[DataRequired(), Length(min = 4, max = 8)])
-    password         = PasswordField("Password", validators=[DataRequired(), Length(min = 8)])
-    email            = EmailField("Email", validators=[DataRequired(), Length(min = 8)])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password", message = "Password must match!")])
-    submit           = SubmitField("Sign Up")
+    username             = StringField("username", validators=[DataRequired(), Length(min=4, max=12), exists_username])
+    fullname             = StringField("full name", validators=[DataRequired(), Length(min=4, max=16)])
+    email                = EmailField("email", validators=[DataRequired(), Email(), exists_email])
+    password             = PasswordField("password", validators=[DataRequired(), Length(min=8)])
+    confirm_password     = PasswordField("confirm chpassword", validators=[DataRequired(), Length(min=8), EqualTo("password")])
+    submit               = SubmitField("sign up")
 
-class EditProfile(FlaskForm):
-    username         = StringField("Username", validators=[DataRequired(), Length(min = 4, max = 8)])
-    new_name         = TextAreaField("Name")
-    new_bio          = TextAreaField("New Bio")
-    new_profile_pic  = TextAreaField("New Profile Picture")
-    submit           = SubmitField("Update Profile")
+class EditProfileForm(FlaskForm):
+    username             = StringField("username", validators=[DataRequired(), Length(min=4, max=12), exists_username])
+    email                = EmailField("email", validators=[DataRequired(), Email(), exists_email])
+    profile_pic          = FileField("profile picture", validators=[FileAllowed(["jpg", "png", "jpeg"])])
+    password             = PasswordField("password", validators=[DataRequired()])
+    submit               = SubmitField("update profile")
 
-class CreatePost(FlaskForm):
-    post    = TextAreaField("Post")
-    caption = TextAreaField("Caption")
-    submit  = SubmitField("Create Post")
+class ResetPasswordForm(FlaskForm):
+    old_password         = PasswordField("old password", validators=[DataRequired(), Length(min=8)])
+    new_password         = PasswordField("new password", validators=[DataRequired(), Length(min=8)])
+    confirm_new_password = PasswordField("confirm new password", validators=[DataRequired(), Length(min=8), EqualTo("new_password")])
+    submit               = SubmitField("reset password")
 
-class EditPost(FlaskForm):
-    new_caption = TextAreaField("New Caption")
-    submit      = SubmitField("Update Post")
+class ForgotPasswordForm(FlaskForm):
+    email                = PasswordField("email", validators=[DataRequired(), not_exists_email])
+    recaptcha            = RecaptchaField()
+    submit               = SubmitField("send link verification to email")
+
+class VerificationResetPasswordForm(FlaskForm):
+    password             = PasswordField("new password", validators=[DataRequired(), Length(min = 8)])
+    confirm_password     = PasswordField("confirm new password", validators=[DataRequired, Length(min = 8), EqualTo("password")])
+    submit               = SubmitField("reset password")
+
+
+class CreatePostForm(FlaskForm):
+    post_pic             = FileField("picture", validators=[DataRequired(), FileAllowed(["jpg", "png", "jpeg"])])
+    caption              = TextAreaField("caption")
+    submit               = SubmitField("post")
+
+class EditPostForm(FlaskForm):
+    caption              = StringField("caption")
+    submit               = SubmitField("update post")
--- application/models.py
+++ application/models.py
@@ -1,50 +1,59 @@
+from flask_login import UserMixin
+
 from app import db
 from datetime import datetime
 
-class User(db.Model):
-    __tablename__ = "users"
-    id          = db.Column(db.Integer, primary_key = True)
-    username    = db.Column(db.String(128), nullable = False)
-    email       = db.Column(db.String(128), nullable = False)
-    password    = db.Column(db.String(128), nullable = False)
-    fullname    = db.Column(db.String(128), nullable = False)
-    profile_pic = db.Column(db.String(128), default = "default.jpg")
-    bio         = db.Column(db.String(128))
-    join_date   = db.Column(db.DateTime, default = datetime.utcnow)
-    status      = db.Column(db.Boolean, default = True)
+class User(db.Model, UserMixin):
+    __tablename__   = "users"
+    id              = db.Column(db.Integer, primary_key = True)
+    username        = db.Column(db.String(128), nullable = False)
+    password        = db.Column(db.String(128), nullable = False)
+    fullname        = db.Column(db.String(128), nullable = False)
+    email           = db.Column(db.String(128), nullable = False)
+    profile_pic     = db.Column(db.String(128), default="default.jpg")
+    bio             = db.Column(db.String(128))
+    join_date       = db.Column(db.DateTime, default=datetime.utcnow)
+    status          = db.Column(db.Boolean, default=True)
+    following_users = db.relationship("Relation", foreign_keys="Relation.id_following", backref="following", lazy=True)
+    follower_users  = db.relationship("Relation", foreign_keys="Relation.id_follower", backref="follower", lazy=True)
+    posts           = db.relationship("Post", backref="posts_owner", lazy=True)
+    likes           = db.relationship("Like", backref="likes_owner", lazy=True)
+    comments        = db.relationship("Comment", backref="comment_owner", lazy=True)
+
 
 class Relation(db.Model):
-    __tablename__ = "relations"
-    id            = db.Column(db.Integer, primary_key = True)
-    id_follower   = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    id_following  = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    status        = db.Column(db.Boolean, default = True)
-    relation_date = db.Column(db.DateTime, default = datetime.utcnow)
+    __tablename__   = "relations"
+    id              = db.Column(db.Integer, primary_key = True)
+    id_follower     = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    id_following    = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    status          = db.Column(db.Boolean, default=True)
+    relation_date   = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Post(db.Model):
-    __tablename__ = "posts"
-    id           = db.Column(db.Integer, primary_key = True)
-    author_id    = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    photo        = db.Column(db.String(128), nullable = False)
-    caption      = db.Column(db.String(128), default = "")
-    status       = db.Column(db.Boolean, default = True)
-    post_date    = db.Column(db.DateTime, default = datetime.utcnow)
+    __tablename__   = "posts"
+    id              = db.Column(db.Integer, primary_key = True)
+    author_id       = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    photo           = db.Column(db.String(128), nullable = False)
+    caption         = db.Column(db.String(128), default="")
+    status          = db.Column(db.Boolean, default=True)
+    post_date       = db.Column(db.DateTime, default=datetime.utcnow)
+    comments        = db.relationship("Comment", backref="commented", lazy=True)
+    likes           = db.relationship("Like", backref="liked", lazy=True)
 
 
 class Comment(db.Model):
-    __tablename__ = "comments"
-    id           = db.Column(db.Integer, primary_key = True)
-    text         = db.Column(db.Text, nullable = False)
-    commenter_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    post_id      = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable = False)
-    hidden       = db.Column(db.Boolean, default = False)
-    comment_date = db.Column(db.DateTime, default = datetime.utcnow)
-
+    __tablename__   = "comments"
+    id              = db.Column(db.Integer, primary_key = True)
+    text            = db.Column(db.Text, nullable = False)
+    commenter_id    = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id         = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    hidden          = db.Column(db.Boolean, default=False)
+    comment_date    = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Like(db.Model):
-    __tablename__ = "likes"
-    id        = db.Column(db.Integer, primary_key = True)
-    user_id   = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-    post_id   = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable = False)
-    status    = db.Column(db.Boolean, default = True)
-    like_date = db.Column(db.DateTime, default = datetime.utcnow)
+    __tablename__   = "likes"
+    id              = db.Column(db.Integer, primary_key = True)
+    user_id         = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+    post_id         = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable = False)
+    status          = db.Column(db.Boolean, default=True)
+    like_date       = db.Column(db.DateTime, default=datetime.utcnow)
