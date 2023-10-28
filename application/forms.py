96637255357f31deeba4571d79472e6a05e2c951
arv <marvin07ar@gmail.com>
2023-10-28 21:11:27 +0700

    28/10/2023

--- application/forms.py
+++ application/forms.py
@@ -7,47 +7,47 @@ from application.utils import exists_email, not_exists_email, exists_username
     
 
 class LoginForm(FlaskForm):
-    username             = StringField("username", validators=[DataRequired()])
-    password             = PasswordField("password", validators=[DataRequired()])
-    submit               = SubmitField("login")
+    username             = StringField("Username", validators=[DataRequired()])
+    password             = PasswordField("Password", validators=[DataRequired()])
+    submit               = SubmitField("Login")
 
 class SignUpForm(FlaskForm):
-    username             = StringField("username", validators=[DataRequired(), Length(min=4, max=12), exists_username])
-    fullname             = StringField("full name", validators=[DataRequired(), Length(min=4, max=16)])
-    email                = EmailField("email", validators=[DataRequired(), Email(), exists_email])
-    password             = PasswordField("password", validators=[DataRequired(), Length(min=8)])
-    confirm_password     = PasswordField("confirm chpassword", validators=[DataRequired(), Length(min=8), EqualTo("password")])
-    submit               = SubmitField("sign up")
+    username             = StringField("Username", validators=[DataRequired(), Length(min=4, max=12), exists_username])
+    fullname             = StringField("Full name", validators=[DataRequired(), Length(min=4, max=16)])
+    email                = EmailField("E-mail", validators=[DataRequired(), Email(), exists_email])
+    password             = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    confirm_password     = PasswordField("Confirm password", validators=[DataRequired(), Length(min=8), EqualTo("password")])
+    submit               = SubmitField("Sign Up")
 
 class EditProfileForm(FlaskForm):
-    username             = StringField("username", validators=[DataRequired(), Length(min=4, max=12), exists_username])
-    email                = EmailField("email", validators=[DataRequired(), Email(), exists_email])
-    profile_pic          = FileField("profile picture", validators=[FileAllowed(["jpg", "png", "jpeg"])])
-    password             = PasswordField("password", validators=[DataRequired()])
-    submit               = SubmitField("update profile")
+    username             = StringField("Username", validators=[DataRequired(), Length(min=4, max=12), exists_username])
+    email                = EmailField("E-mail", validators=[DataRequired(), Email(), exists_email])
+    profile_pic          = FileField("Profile picture", validators=[FileAllowed(["jpg", "png", "jpeg"])])
+    password             = PasswordField("Password", validators=[DataRequired()])
+    submit               = SubmitField("Update profile")
 
 class ResetPasswordForm(FlaskForm):
-    old_password         = PasswordField("old password", validators=[DataRequired(), Length(min=8)])
-    new_password         = PasswordField("new password", validators=[DataRequired(), Length(min=8)])
-    confirm_new_password = PasswordField("confirm new password", validators=[DataRequired(), Length(min=8), EqualTo("new_password")])
-    submit               = SubmitField("reset password")
+    old_password         = PasswordField("Old password", validators=[DataRequired(), Length(min=8)])
+    new_password         = PasswordField("New password", validators=[DataRequired(), Length(min=8)])
+    confirm_new_password = PasswordField("Confirm new password", validators=[DataRequired(), Length(min=8), EqualTo("new_password")])
+    submit               = SubmitField("Reset password")
 
 class ForgotPasswordForm(FlaskForm):
-    email                = PasswordField("email", validators=[DataRequired(), not_exists_email])
-    recaptcha            = RecaptchaField()
-    submit               = SubmitField("send link verification to email")
+    email                = PasswordField("E-mail", validators=[DataRequired(), not_exists_email])
+    # recaptcha            = RecaptchaField()
+    submit               = SubmitField("Send link verification to e-mail")
 
 class VerificationResetPasswordForm(FlaskForm):
-    password             = PasswordField("new password", validators=[DataRequired(), Length(min = 8)])
-    confirm_password     = PasswordField("confirm new password", validators=[DataRequired(), Length(min = 8), EqualTo("password")])
-    submit               = SubmitField("reset password")
+    password             = PasswordField("New password", validators=[DataRequired(), Length(min = 8)])
+    confirm_password     = PasswordField("Confirm new password", validators=[DataRequired(), Length(min = 8), EqualTo("password")])
+    submit               = SubmitField("Reset password")
 
 
 class CreatePostForm(FlaskForm):
-    post_pic             = FileField("picture", validators=[DataRequired(), FileAllowed(["jpg", "png", "jpeg"])])
-    caption              = TextAreaField("caption")
-    submit               = SubmitField("post")
+    post_pic             = FileField("Picture", validators=[DataRequired(), FileAllowed(["jpg", "png", "jpeg"])])
+    caption              = TextAreaField("Caption")
+    submit               = SubmitField("Post")
 
 class EditPostForm(FlaskForm):
-    caption              = StringField("caption")
-    submit               = SubmitField("update post")
+    caption              = StringField("Caption")
+    submit               = SubmitField("Update post")
--- application/routes.py
+++ application/routes.py
@@ -8,7 +8,7 @@ from application.forms import *
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('protected'))
+        return redirect(url_for('profile'))
 
     form = LoginForm()
 
@@ -19,11 +19,11 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and password == user.password:
             login_user(user)
-            return redirect(url_for('protected'))
+            return redirect(url_for('profile'))
         else:
             flash('Invalid username or password', 'error')
 
-    return render_template('login.html', form=form)
+    return render_template('login.html', title="Login", form=form)
 
 @app.route('/logout')
 @login_required
@@ -31,14 +31,55 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/protected')
+@app.route('/profile')
 @login_required
-def protected():
-    return 'This is a protected page. You are logged in as ' + current_user.username
+def profile():
+    return render_template('profile.html', title=f'{current_user.fullname} Profile')
 
 @app.route('/')
+@login_required
 def index():
-    return 'ok'
+    return render_template('index.html', title='Home')
+
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', title='SignUp', form=form)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/forgot')
+def forgot():
+    form = ForgotPasswordForm()
+    return render_template('forgot_password.html', title='Forgot', form = form)
+
+@app.route('/edit_profile')
+def edit_profile():
+    form = EditProfileForm()
+    return render_template('profile_edit.html', title='Edit Profile', form = form)
+
+@app.route('/reset')
+def reset():
+    form = ResetPasswordForm()
+    return render_template('reset_password.html', title='Reset', form = form)
+
+@app.route('/verif')
+def verif():
+    form = VerificationResetPasswordForm()
+    return render_template('verif.html', title='Verif', form = form)
+
+@app.route('/create_post')
+def create():
+    form = CreatePostForm()
+    return render_template('create_post.html', title='Create', form = form)
+
+@app.route('/edit_post')
+def edit_post():
+    form = EditPostForm()
+    return render_template('edit_post.html', title='Edit Post', form = form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
