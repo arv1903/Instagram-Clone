35e5e2787e541167bab362ec047b686f350ac9c8
arv <marvin06ar@gmail.com>
2023-11-17 13:51:41 +0700

    Fixed like bug

--- application/routes.py
+++ application/routes.py
@@ -188,16 +188,16 @@ def edit_post(id):
 def like():
     data = request.json
     post_id = int(data['postId'])
-    like = Like.query.filter_by(user_id = current_user, post_id=post_id).first()
+    like = Like.query.filter_by(user_id=current_user.id,post_id=post_id).first()
     if not like:
         like = Like(user_id=current_user.id, post_id=post_id)
         db.session.add(like)
         db.session.commit()
-        return make_response(200, jsonify({"status" : True}), 200)
+        return make_response(jsonify({"status" : True}), 200)
     
     db.session.delete(like)
     db.session.commit()
-    return make_response(200, jsonify({"status" : False}), 200)
+    return make_response(jsonify({"status" : False}), 200)
 
 if __name__ == '__main__':
     app.run(debug=True) 
